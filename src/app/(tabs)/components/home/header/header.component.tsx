3220139a728bc32561b1573012562078c95c9a73
AugustoCVS <guty.vertolis@gmail.com>
2025-05-18 23:46:54 -0300

    feat: add logout

--- src/app/(tabs)/components/home/header/header.component.tsx
+++ src/app/(tabs)/components/home/header/header.component.tsx
@@ -1,20 +1,32 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { Input } from "@/components/inputs/input/input.component";
 import { colors } from '@/styles/colors';
 import { Text, View } from "react-native";
+import { useHeader } from './header.hook';
 import { HeaderProps } from "./header.types";
 
 export const Header: React.FC<HeaderProps> = ({name, value, setValue}) => {
+  const { actions } = useHeader();
+
   return (
     <View
       className="w-full flex flex-col items-start justify-center h-52 bg-gray-700 pt-10 px-4"
     >
-      <Text
-        className="text-2xl font-bold text-white-100"
+      
+      <View
+        className='flex flex-row items-center justify-between w-full'
       >
-        Bem vindo, {name}!
-      </Text>
+        <Text
+          className="text-2xl font-bold text-white-100"
+        >
+          Bem vindo, {name}!
+        </Text>
+
+        <MaterialIcons name="logout" size={24} color="white" onPress={actions.handleLogout} />
+
+      </View>
 
       <Input 
         placeholder="Pesquisar"
